50a6b6bb5150a79f57bee9b9cfca176896ab56e4
nathsteve13 <nathanaelsteven06@gmail.com>
2025-04-13 16:38:26 +0700

    Bootstap apply (done)

---
+++ resources/views/layouts/app.blade.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+@include('layouts.header')
+
+<body>
+
+    @include('layouts.svg-symbols')
+
+    <div class="preloader-wrapper">
+        <div class="preloader">
+        </div>
+    </div>
+
+    @include('layouts.offcanvas-cart')
+
+    @include('layouts.offcanvas-search')
+
+
+    @include('layouts.navbar')
+
+    <main>
+        @yield('content')
+    </main>
+
+    @include('layouts.footer')
+
+    <script src="js/jquery-1.11.0.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/swiper@9/swiper-bundle.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
+    </script>
+    <script src="js/plugins.js"></script>
+    <script src="js/script.js"></script>
+</body>
+
+</html>
