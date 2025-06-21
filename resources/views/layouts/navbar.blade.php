a18b71d736ecaf0e0564bbaea418c3d97af0bb9e
cennialieta <s160422037@student.ubaya.ac.id>
2025-06-21 12:59:14 +0700

    home

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -24,7 +24,7 @@
                     <div class="col-11 col-md-7">
                         <form id="search-form" class="text-center" action="index.html" method="post">
                             <input type="text" class="form-control border-0 bg-transparent"
-                                placeholder="Search for more than 20,000 products" />
+                                placeholder="Search for more products" />
                         </form>
                     </div>
                     <div class="col-1">
@@ -36,12 +36,6 @@
                 </div>
             </div>
 
-            <div
-                class="col-sm-8 col-lg-4 d-flex justify-content-end gap-5 align-items-center mt-4 mt-sm-0 justify-content-center justify-content-sm-end">
-                <div class="support-box text-end d-none d-xl-block">
-                    <span class="fs-6 text-muted">For Support?</span>
-                    <h5 class="mb-0">+980-34984089</h5>
-                </div>
 
                 <ul class="d-flex justify-content-end list-unstyled m-0">
                     <li>
@@ -80,7 +74,6 @@
                     <button class="border-0 bg-transparent d-flex flex-column gap-2 lh-1" type="button"
                         data-bs-toggle="offcanvas" data-bs-target="#offcanvasCart" aria-controls="offcanvasCart">
                         <span class="fs-6 text-muted dropdown-toggle">Your Cart</span>
-                        <span class="cart-total fs-5 fw-bold">$1290.00</span>
                     </button>
                 </div>
             </div>
@@ -107,52 +100,19 @@
 
                         <div class="offcanvas-body">
 
-                            <select class="filter-categories border-0 mb-0 me-5">
-                                <option>Shop by Departments</option>
-                                <option>Groceries</option>
-                                <option>Drinks</option>
-                                <option>Chocolates</option>
-                            </select>
 
                             <ul class="navbar-nav justify-content-end menu-list list-unstyled d-flex gap-md-3 mb-0">
                                 <li class="nav-item active">
-                                    <a href="#women" class="nav-link">Women</a>
+                                    <a href="#makananberat" class="nav-link">Makanan Berat</a>
                                 </li>
                                 <li class="nav-item dropdown">
-                                    <a href="#men" class="nav-link">Men</a>
+                                    <a href="#minuman" class="nav-link">Minuman</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a href="#kids" class="nav-link">Kids</a>
+                                    <a href="#cemilan" class="nav-link">Cemilan</a>
                                 </li>
                                 <li class="nav-item">
-                                    <a href="#accessories" class="nav-link">Accessories</a>
-                                </li>
-                                <li class="nav-item dropdown">
-                                    <a class="nav-link dropdown-toggle" role="button" id="pages"
-                                        data-bs-toggle="dropdown" aria-expanded="false">Pages</a>
-                                    <ul class="dropdown-menu" aria-labelledby="pages">
-                                        <li><a href="index.html" class="dropdown-item">About Us </a></li>
-                                        <li><a href="index.html" class="dropdown-item">Shop </a></li>
-                                        <li><a href="index.html" class="dropdown-item">Single Product </a></li>
-                                        <li><a href="index.html" class="dropdown-item">Cart </a></li>
-                                        <li><a href="index.html" class="dropdown-item">Checkout </a></li>
-                                        <li><a href="index.html" class="dropdown-item">Blog </a></li>
-                                        <li><a href="index.html" class="dropdown-item">Single Post </a></li>
-                                        <li><a href="index.html" class="dropdown-item">Styles </a></li>
-                                        <li><a href="index.html" class="dropdown-item">Contact </a></li>
-                                        <li><a href="index.html" class="dropdown-item">Thank You </a></li>
-                                        <li><a href="index.html" class="dropdown-item">My Account </a></li>
-                                        <li><a href="index.html" class="dropdown-item">404 Error </a></li>
-                                    </ul>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="#brand" class="nav-link">Brand</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="#sale" class="nav-link">Sale</a>
-                                </li>
-                                <li class="nav-item">
-                                    <a href="#blog" class="nav-link">Blog</a>
+                                    <a href="#makananpenutup" class="nav-link">Makanan Penutup</a>
                                 </li>
                             </ul>
 
